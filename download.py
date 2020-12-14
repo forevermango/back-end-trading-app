bd28ea50f76b3754c1c49fa928c36e948249cc01
forevermango <raahima95@gmail.com>
2020-12-14 13:53:58 -0600

    creating tables for database

--- download.py
+++ download.py
@@ -1,5 +1,5 @@
 import yfinance
 
-data_frame = yfinance.download('AAPL', start='2020-01-01', end='2020-10-02')
+data_frame = yfinance.download('AAPL', start='2020-01-01', end='2020-11-02')
 data_frame.to_csv('AAPL.csv')
 
