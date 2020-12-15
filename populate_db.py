f016a0fe09a71df76bb61efb16117815a4ee084a
forevermango <raahima95@gmail.com>
2020-12-14 19:08:30 -0600

    added alpaca api key and created assetsquit

--- populate_db.py
+++ populate_db.py
@@ -1,9 +1,24 @@
 import sqlite3
+import alpaca_trade_api as tradeapi
+
 
 connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
 
-cursor.execute("DELETE FROM stock")
+
+api = tradeapi.REST('PKG1VWGGEU5M413AP0OC', 'ctSIUS7HX2zTaXQDmLgqZeFBiUsdwsDkoFmGOZBV', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+assets = api.list_assets()
+for asset in assets:
+
+    try:
+        cursor.execute("""
+            INSERT INTO stock (symbol, name, exchange)
+            VALUES (?, ?, ?)
+        """, (asset.symbol, asset.name, asset.exchange))
+    except Exception as e:
+        print(e)
+        print(asset)
+
 
 connection.commit()
