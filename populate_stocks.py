fb93626496e3d923d2f92f805c0cab988a5cc036
forevermango <raahima95@gmail.com>
2020-12-16 14:51:00 -0600

    fixed bug in config.db file

--- populate_stocks.py
+++ populate_stocks.py
@@ -2,7 +2,7 @@ import sqlite3, config
 import alpaca_trade_api as tradeapi
 
 
-connection = sqlite3.connect('app.db')
+connection = sqlite3.connect('config.DB_FILE')
 connection.row_factory = sqlite3.Row
 
 cursor = connection.cursor()
@@ -15,7 +15,7 @@ rows = cursor.fetchall()
 symbols = [row['symbol'] for row in rows]
 
 
-api = tradeapi.REST('config.API_KEY', 'config.SECRET_KEY', base_url='config.API_URL') # or use ENV Vars shown below
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL) # or use ENV Vars shown below
 
 assets = api.list_assets()
 
