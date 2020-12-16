16aa7135ee7cad5ea61b4fbb2f17098ffe6f4f5c
forevermango <raahima95@gmail.com>
2020-12-15 18:37:08 -0600

    interating over assets in row form

--- populate_db.py
+++ populate_db.py
@@ -3,19 +3,26 @@ import alpaca_trade_api as tradeapi
 
 
 connection = sqlite3.connect('app.db')
+connection.row_factory = sqlite3.Row
 
 cursor = connection.cursor()
 
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+rows = cursor.fetchall()
+symbols = [row['symbol'] for row in rows]
+print(symbols)
 
 api = tradeapi.REST('PKG1VWGGEU5M413AP0OC', 'ctSIUS7HX2zTaXQDmLgqZeFBiUsdwsDkoFmGOZBV', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
 assets = api.list_assets()
-for asset in assets:
-    try:
-        if asset.status == 'active' and asset.tradable:
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
-    except Exception as e:
-        print(asset.symbol)
-        print(e)
+#for asset in assets:
+#     try:
+#        if asset.status == 'active' and asset.tradable:
+#            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+#    except Exception as e:
+#        print(asset.symbol)
+#        print(e)
 
 
 
